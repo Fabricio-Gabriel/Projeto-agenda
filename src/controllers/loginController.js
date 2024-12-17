c7a78714660ae40a8285fbd97d7694fa4eac669e
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-12-17 15:19:19 -0300

    Validando criação de contas

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,10 @@
+const Login = require('../controllers/loginController');
+
 exports.index = (req, res) => {
     res.render('login');
 };
 
 exports.register = (req, res) => {
-    res.render('register');
+    const login = new Login(req.body);
+    res.send(req.body);
 };
