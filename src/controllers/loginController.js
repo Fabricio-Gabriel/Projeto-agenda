a2ff539324e787e367a58ad5258da6ceb6febbd9
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-12-18 19:04:01 -0300

    Criando validação de senha e user para login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/loginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+    if (req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
 exports.register = async (req, res) => {
@@ -31,3 +32,38 @@ exports.register = async (req, res) => {
         return res.render('404');
     }
 };
+
+
+exports.login = async (req, res) => {
+    try{
+        const login = new Login(req.body);
+        await login.login();
+
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => {
+                const url = req.get("Referer") || "/login/index";
+                return res.redirect(url);
+            });
+
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema!');
+        req.session.user = login.user;
+        req.session.save(() => {
+            const url = req.get("Referer") || "/login/index";
+            return res.redirect(url);
+        });
+
+        
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+}
